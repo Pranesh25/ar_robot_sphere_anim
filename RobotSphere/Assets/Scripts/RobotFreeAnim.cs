a64ebddb69694375e78f234641a67ae215a74092
Pranesh25 <41353968+Pranesh25@users.noreply.github.com>
2020-10-13 01:01:37 +0530

    Add files via upload

---
+++ RobotSphere/Assets/Scripts/RobotFreeAnim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotFreeAnim : MonoBehaviour {
+
+
+	Animator anim;
+
+	// Use this for initialization
+	void Awake()
+	{
+		anim = gameObject.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+
+    //Functions to animated the character
+    public void Walk()
+    {
+        anim.SetBool("Walk_Anim", true);
+    }
+
+    public void Roll()
+    {
+        anim.SetBool("Roll_Anim", true);
+    }
+
+    public void Stay()
+    {
+        anim.SetBool("Walk_Anim", false);
+        anim.SetBool("Roll_Anim", false);
+    }
+  
+    
+}
